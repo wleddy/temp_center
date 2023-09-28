88a93fed8df0bc0ed15b0489f96566eb44f25f69
Bill Leddy <bill@williesworkshop.net>
2023-09-27 17:11:37 -0700

    Remove pointless comment

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from shotglass2.users.models import User
 from temp_center.models import init_db as temp_center_init, Device, Sensor, Reading
 from temp_center.views import home, device, sensor, reading, api
 
-# add a pointless comment
 # Create app
 import logging 
 try:

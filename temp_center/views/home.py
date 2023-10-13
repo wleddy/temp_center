1bb410e01e50ffcb093aae89ac5575f582bcdb32
Bill Leddy <bill@williesworkshop.net>
2023-10-13 11:11:04 -0700

    display min and max temps

--- temp_center/views/home.py
+++ temp_center/views/home.py
@@ -1,6 +1,7 @@
 from flask import request, session, g, redirect, url_for, \
      render_template, flash, Blueprint
 from temp_center.models import Sensor, Device, Reading
+from datetime import datetime
 
 mod = Blueprint('home',__name__, template_folder='templates/', url_prefix='')
 
@@ -20,17 +21,22 @@ def home():
     sql = """
     select reading.id, round(temperature,1) as temperature, scale, 
         device.name as device_name, sensor.name as sensor_name, 
-        (select max(reading_time) from reading) as reading_time
+        (select max(reading_time) from reading) as reading_time,
+        (select round(max(temperature),0) from reading where sensor_id = {sensor_id} 
+            and date(reading_time,'localtime') = '{today}') as max_temp,
+        (select round(min(temperature),0) from reading where sensor_id = {sensor_id} 
+            and date(reading_time, 'localtime') = '{today}') as min_temp
     from reading
     join sensor on sensor.id = reading.sensor_id
     join device on device.id = sensor.device_id
-    where device.id = {device_id} and sensor.id = {sensor_id}
+    where sensor.id = {sensor_id}
     order by reading.reading_time DESC
     limit 1
     """
 
     data = []
     sensors = []
+    today = datetime.now().strftime('%Y-%m-%d')
     # import pdb;pdb.set_trace()
     devices = Device(g.db).select(order_by="name")
     if devices:
@@ -38,7 +44,7 @@ def home():
             sensors = Sensor(g.db).select(where="device_id = {}".format(device.id),order_by="name DESC")
             if sensors:
                 for sensor in sensors:
-                    readings = Reading(g.db).query(sql.format(device_id = device.id, sensor_id = sensor.id))
+                    readings = Reading(g.db).query(sql.format(sensor_id = sensor.id,today=today))
                     if readings:
                         data.append(readings[0])
 

50146f93362cd3cb67000fb22c68c7dc106f1ab7
Bill Leddy <bill@williesworkshop.net>
2026-04-21 11:16:22 -0700

    restore the request part

--- enphase/local-api/enphase_token_fetch.py
+++ enphase/local-api/enphase_token_fetch.py
@@ -23,10 +23,10 @@ user=creds["user"]
 password=creds['password']
 envoy_serial=creds['envoy_serial']
 data = {'user[email]': user, 'user[password]': password}
-print(data)
-# response = requests.post('http://enlighten.enphaseenergy.com/login/login.json?', data=data)
-# response_data = json.loads(response.text)
-# data = {'session_id': response_data['session_id'], 'serial_num': envoy_serial, 'username': user}
-# response = requests.post('http://entrez.enphaseenergy.com/tokens', json=data)
-# token_raw = response.text
-# print(token_raw)
+# print(data)
+response = requests.post('http://enlighten.enphaseenergy.com/login/login.json?', data=data)
+response_data = json.loads(response.text)
+data = {'session_id': response_data['session_id'], 'serial_num': envoy_serial, 'username': user}
+response = requests.post('http://entrez.enphaseenergy.com/tokens', json=data)
+token_raw = response.text
+print(token_raw)
